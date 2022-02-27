f621721b8dcb307289290e41e6735bbd77a5f3e9
Meghna Singh <meghnaq223@gmail.com>
2022-02-27 11:40:29 +0530

    minor bug fixes and final commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,19 @@ import {Route, BrowserRouter,Routes} from 'react-router-dom';
 
 import HomePage from './components/HomePage'
 import HospitalRegistration from './components/HospitalRegistration';
-import UserRegistration from './components/UserRegistration';
-import DoctorRegistration from './components/DoctorRegistration';
 import UserSearch from './components/UserSearch';
+import UserRegistration from './components/UserRegistration';
+import Appointment from './components/Appointment';
+import DoctorBoard from './components/DoctorBoard';
+import LoginUi from './components/LoginUi'
 import Resources from './components/Resources';
-import LoginUi from './components/LoginUi';
-import Diabetes from './components/Diabetes';
-import PCOSPrediction from './components/PCOSPrediction';
-import DepressionForm from './components/DepressionForm';
-import ChronicDiagnosis from './components/ChronicDiagnosis';
+import  Doctor  from './components/Doctor';
 import Symptom from './components/Symptom';
+import DoctorRegistration from './components/DoctorRegistration';
+import PCOSPrediction from './components/PCOSPrediction';
+import Diabetes from './components/Diabetes';
+import DepressionForm from './components/DepressionForm';
+import ChronicDiagnosis from './components/ChronicDiagnosis'
 function App() {
   return (
     <BrowserRouter>
@@ -23,18 +26,19 @@ function App() {
       
        <Route path="/" exact element={<HomePage />} />
        <Route path="/HospitalRegistration" element={<HospitalRegistration />} /> 
+       <Route path="/usersearch" element={<UserSearch />} />
        <Route path="/UserRegistration" element={<UserRegistration />} /> 
-       <Route path="/DoctorRegistration" element={<DoctorRegistration />} />
-       <Route path="/usersearch" element={<UserSearch />} /> 
-       <Route path='/resources' element={<Resources/>}/>
+       <Route path="/appointment/:id" element={<Appointment />} /> 
+       <Route path="/dashboard" element={<DoctorBoard/>}/>
        <Route path='login' element={<LoginUi/>}/>
-       <Route path="/diabetes" element={<Diabetes/>}/>
-       <Route path="/pcos" element={<PCOSPrediction/>} />
+       <Route path='/resources' element={<Resources/>}/>
+       <Route path='/doctors' element={<Doctor/>}/>
        <Route path="/symptoms" element={<Symptom/>} />
+       <Route path="/doctorRegistration" element={<DoctorRegistration/>} />
+       <Route path="/pcos" element={<PCOSPrediction/>} />
+       <Route path="/diabetes" element={<Diabetes/>}/>
        <Route path="/depression" element={<DepressionForm/>} />
        <Route path="/chronic" element={<ChronicDiagnosis/>} />
-       
-       
        
          
        </Routes>
